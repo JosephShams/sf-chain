07cc1d0f2fa94e11d1c23fc2cd62a47249cfb3e3
Yashar <joseph.shams@kcl.ac.uk>
2018-07-06 14:50:56 +0100

    tested transaction verification

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -20,8 +20,17 @@ describe('Transaction', () => {
         expect(transaction.outputs.find(output => output.address === recipient).amount).toEqual(amount);
     });
 
-    it('inouts the balance of the wallet', () => {
+    it('inputs the balance of the wallet', () => {
         expect(transaction.input.amount).toEqual(wallet.balance);
+    });
+
+    it('validates a valid transction', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true);
+    });
+
+    it('validates a corrupt transaction', () => {
+        transaction.outputs[0].amount = 50000
+        expect(Transaction.verifyTransaction(transaction)).toBe(false);
     });
 
     describe('transacting with an amount that exceeds the balance', () => {
